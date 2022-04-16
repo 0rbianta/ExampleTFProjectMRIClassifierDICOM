900d4d7503c6190c7ca653098d9b995eb38b5b99
0rbianta\nAlso known as /dev/null <63746411+0rbianta@users.noreply.github.com>
2022-04-16 22:55:36 +0300

    Fonts, output, neuralRunner, cli etc.

--- main.py
+++ main.py
@@ -1,43 +1,68 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img
-import numpy as np
+from random import choice as randchoice
+from time import sleep, time
+from PIL import ImageDraw, ImageFont
+from neural import neuralRunner
 
-net = load_model("./keras_model.h5")
+neuralRunner = neuralRunner("./keras_model.h5", "labelmap.txt")
+imgDrawFont = "./Anonymous_Pro/AnonymousPro-Regular.ttf"
+getMS = lambda : round(time() * 1000)
 
 
-def main():
-    path = input("IMG PATH>> ")
-    path = "./archive/yes/Y4.jpg"
-    img = load_img(
-        path, target_size=(224, 224, 3))
-    np.array(img)
-    img = np.expand_dims(img, axis=0)
-    img = np.vstack([img])
-    network_output = net.predict(img, batch_size=1)
-    print(np.argmax(network_output))
+def showImageOutput(img, label: str):
+    draw = ImageDraw.Draw(img)
+    draw.text((3, 3), "Output: " + label, (255, 0, 0), 
+        font=ImageFont.truetype(imgDrawFont, 17))
+    draw.text((3, 20), "Copyright © 0rbianta", (255, 0, 0), 
+        font=ImageFont.truetype(imgDrawFont, 13))
+    img.show()
 
 
 if __name__ == "__main__":
-    print('''Welcome to AI Brain MRI Tumor Detector App
-            1) Load an Brain MRI Image
-            2) Load an DICOM Folder
-            3) About
-            4) Exit
-            Copyright © 0rbianta / Cem Pişkinpaşa / OrxAI''')
-    
     # App main loop
     while True:
+        print('''Welcome to AI Brain MRI Tumor Detector App
+                1) Load an Brain MRI Image
+                2) Load an DICOM Folder
+                3) About
+                4) Exit
+                Copyright © 0rbianta / Cem Pişkinpaşa / OrxAI''')
+
         usr = input("prompt > ")
+        if usr.isnumeric():
+            usr = int(usr)
         if usr == 1:
-            print("You choose the option for loading an brain MRI data image. Please enter the path of the image.")
-            usr = input("prompt > ")
+            print("You choose the option for loading an brain MRI image. Please enter the path of an image that brains top profile easly visible.")
+            usr = input("prompt > option 1 > ")
+            # Run with an test image for debugging purposes
+            usr = "./dataset/yes/Y4.jpg"
+            print("Executing for image", usr)
+            startMS = getMS()
+            img = neuralRunner.loadImage(usr)
+            if img not in ("0", "1"):
+                classID = neuralRunner.predictWithImage(img)
+                if classID not in ("0", "1"):
+                    print(f"Process complete in {getMS() - startMS} milliseconds.")
+                    outLabel = neuralRunner.labels[classID]
+                    print(f"ClassID: {classID}, Label: {outLabel}")
+                    showImageOutput(img, outLabel)
+                else:
+                    print("Failed to forward image through the network.")
+                    print("Problem:")
+                    print(["    Input shape and networks shape different.",
+                    "   An unknown error happened while forwarding image through the network."][int(classID)])
+                    print("Please contact to the developer for help. His mail is available on the about section.")
+            else:
+                print(f'Failed to load image due to {["file not found", "an unknown error"][int(img)]}.')
 
 
         elif usr == 2:
-            pass
+            print("Not yet implemented.")
         elif usr == 3:
-            pass
-        elif usr == 4:
-            pass
+            print('''This will be the content of about section soon. Please wait for the development process to complete. Also there is my mail, orbianta@protonmail.com''')
+        elif usr == 4 or usr == "exit" or usr == "exit()" or usr == "q":
+            print(randchoice(["👋 Good Bye!", "👋 See you!", "👋 We wish you healthy days!"]))
+            exit(0)
         else:
-            print("Invalid command entered. Please enter a valid number listed in the list")
+            print("🤔 Invalid command entered. Please enter a valid number listed in the list.")
+
+        sleep(1)
--- neural.py
+++ neural.py
@@ -1,9 +1,37 @@
-from cProfile import label
-from statistics import mode
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img
+import numpy as np
 
 
 class neuralRunner:
     def __init__(self, modelPath, labelPath):
-        self.modelpath = modelPath
-        self.labelPath = labelPath
+        self.net = load_model(modelPath)
+        self.__load_labels(labelPath)
     
+    def __load_labels(self, lpath):
+        file = open(lpath, "r")
+        self.labels = file.read().split("\n")
+        file.close()
+
+    def loadImage(self, imgPath):
+        try:
+            return load_img(imgPath, target_size=(224, 224, 3))
+        except FileNotFoundError:
+            return "0"
+        except:
+            return "1"
+
+
+    def predictWithImage(self, img):
+        np.array(img)
+        img = np.expand_dims(img, axis=0)
+        img = np.vstack([img])
+        try:
+            net_output = self.net.predict(img, batch_size=1)
+            return np.argmax(net_output)
+        except ValueError:
+            return "0"
+        except:
+            return "1"
+
+
